0ba854ad19f0b5c6444385b63470f53090c369b1
Mutmainah <mutmainnahadewoye@gmail.com>
2023-09-26 21:37:06 -0800

    Counter App

---
+++ script.js
@@ -0,0 +1,15 @@
+const displayoutput = document.getElementById('displayOutput')
+const increment= document.getElementById('increment');
+const decrement= document.getElementById('decrement');
+
+let counter = 0
+
+increment.addEventListener('click', function(){
+    counter--
+    displayOutput .innerText = counter
+})
+
+decrement.addEventListener('click', function(){
+    counter++
+    displayOutput .innerText = counter
+})
